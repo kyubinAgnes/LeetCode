beca6ec07c1bc148579e64956a86dcabf7c2bfcd
inkblot626 <47111862+inkblot626@users.noreply.github.com>
2023-03-02 22:25:56 +0900

    Time: 35 ms (60.20%), Space: 13.9 MB (44.13%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,8 +1,8 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n == 0:
-            return 0
-        if n == 1:
-            return 1
+        seen = { 0: 0, 1: 1}
         
-        return(self.fib(n-1) + self.fib(n-2))
+        for i in range(2, n+1):
+            seen[i] = seen[i-1] + seen[i-2]
+            
+        return(seen[n])
